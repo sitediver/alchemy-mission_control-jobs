ef42f09d40dbf1c8d21fb79dbc9d4c2bed24e3c1
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2025-03-01 11:21:21 +0100

    Remove MissionControl::Job engine route
    Moved the logic into `Alchemy::Engine.routes.draw` block to mount the route with Alchemy together.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
-Alchemy::MissionControl::Jobs::Engine.routes.draw do
+Alchemy::Engine.routes.draw do
+  namespace :admin, path: Alchemy.admin_path, constraints: Alchemy.admin_constraints do
+    mount MissionControl::Jobs::Engine, at: "/mission_control_jobs"
+  end
 end
