f0230b0adb05ab047351c7dfd744f3a738ec2202
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2025-03-02 18:46:40 +0100

    Adjust the background color of mission control iframe
    Use light grey background color for mission control backend. This way the iframe does not look like an iframe anymore. It is still pretty odd, because the UI differs from Alchemy, but it is good enough for the first implementation. Also the HTML was a bit adjusted to reduce the padding.

--- lib/alchemy/mission_control/jobs/engine.rb
+++ lib/alchemy/mission_control/jobs/engine.rb
@@ -3,11 +3,12 @@ module Alchemy
     module Jobs
       class Engine < ::Rails::Engine
         isolate_namespace Alchemy
-        engine_name 'alchemy_mission_control-jobs'
+        engine_name "alchemy_mission_control-jobs"
 
-        initializer 'alchemy_mission_control-jobs.assets' do |app|
+        initializer "alchemy_mission_control-jobs.assets" do |app|
           app.config.assets.precompile += [
-            'alchemy/mission_control/jobs/alchemy-admin.css'
+            "alchemy/mission_control/jobs/alchemy-admin.css",
+            "alchemy/mission_control/jobs/mission-control.css"
           ]
         end
       end
