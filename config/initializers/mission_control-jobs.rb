26ce6fae62181a2521d1f8d84c0ab3c24da1a14a
Sascha Karnatz <68833+kulturbande@users.noreply.github.com>
2025-12-06 14:49:12 +0100

    Fix mission control application layout
    The layout only worked in the dummy app, because the mission_control-jobs was loaded before alchemy-mission_control-jobs in the dummy Gemfile. This does not work in common applications. Instead extend the mission_control application controller and use the given application layout.

--- config/initializers/mission_control-jobs.rb
+++ config/initializers/mission_control-jobs.rb
@@ -1,4 +1,5 @@
 Rails.application.config.to_prepare do
   MissionControl::Jobs.base_controller_class = "Alchemy::Admin::JobsController"
   MissionControl::Jobs.http_basic_auth_enabled = false
+  MissionControl::Jobs::ApplicationController.layout "admin/application"
 end
